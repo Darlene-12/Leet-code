cd68bc0701b6842b94c632584e541f8414a014d2
Darlene <98302606+Darlene-12@users.noreply.github.com>
2025-02-05 08:20:02 +0300

    Unique Arrays.py

--- Unique Arrays.py
+++ Unique Arrays.py
@@ -1,1 +1,6 @@
-
+class Solution:
+  def isArraySpecial(self, nums):
+    for i in range(len(num) -1 ):
+      if (nums[i] % 2) == (nums[i+1] % 2): #  Code to iterate over adjacent elements
+        return False
+    return True
